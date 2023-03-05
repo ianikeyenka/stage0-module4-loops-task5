fb213cef7d86deee1da1240c69fa2f2c6e1a087a
Ilya Anikeyenka <20prorok02@gmail.com>
2023-03-05 18:00:12 +0300

    Solution

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
-
+        for (int i = 0; i < height; i++) {
+            System.out.print("8");
+            for (int j = 0; j < length - 2; j++) {
+                if (i == 0 || i == height - 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("8");
+        }
     }
 }
